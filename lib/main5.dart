06d2017fb3c12975a01a0a817f5c3ccbc3d04601
fahmi27071996 <fahmi.sangengon@gmail.com>
2020-04-24 08:23:44 +0700

    update MD

--- lib/main5.dart
+++ lib/main5.dart
@@ -13,16 +13,10 @@ var dataTouch = [0.0, 0.0];
 var statusTouch1 = false, statusTouch2 = false;
 var result = [0.0, 0.0];
 int data1, data2;
-int dataC = 0, cs = 0;
 // int distance = 0;
 List<int> dataTouchInt = [0, 0];
 
 List<int> deltaTouch = [0, 0];
-
-var mode = ["Stanby", "Drag 1", "Zoom"];
-var selectMode = "Stanby";
-
-var gKanan = false,gKiri=false;
 
 void main() => runApp(MyApp());
 
@@ -52,16 +46,17 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  // int _downCounter = 0;
-  // int _upCounter = 0;
+  int _downCounter = 0;
+  int _upCounter = 0;
   double x = 0.0;
   double y = 0.0;
 
   void _incrementDown(PointerEvent details) {
-    // _updateLocation(details);
+   // _updateLocation(details);
     setState(() {
       deltaTouch[0] = 0;
       deltaTouch[1] = 0;
+      _downCounter++;
       print("start");
       statusTouch1 = true;
 
@@ -78,7 +73,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         // }
       }
 
-      if (statusTouch1 && statusTouch2) {
+      if (statusTouch2 && statusTouch2) {
         dataTouch[1] = details.position.dx;
 
         if (dataTouch[0] > dataTouch[1]) {
@@ -90,18 +85,14 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         }
 
         data1 = dataTouchInt[0];
-        result[0] = dataTouchInt[0].toDouble();
         data2 = dataTouchInt[1];
-        result[1] = dataTouchInt[1].toDouble();
 
-        print("Touch 1 : " + dataTouchInt[0].toInt().toString());
+         print("Touch 1 : " + dataTouchInt[0].toInt().toString());
         print("Touch 2 : " + dataTouchInt[1].toInt().toString());
 
         // if (dataTouch[0] == dataTouch[1]) {
         //   statusTouch2 = false;
         // }
-        // selectMode = mode[0];
-
       }
     });
   }
@@ -109,94 +100,94 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   void _incrementUp(PointerEvent details) {
     //_updateLocation(details);
     setState(() {
-
-      if(gKiri){
-        print("Kiri release");
-        gKiri = false;
-      }
-
-      if(gKanan){
-        print("Kanan Release");
-        gKanan = false;
-      }
-
-
-
-
       dataTouch[0] = 0;
       dataTouch[1] = 0;
       deltaTouch[0] = 0;
       deltaTouch[1] = 0;
-      dataTouchInt[0] = 0;
-      dataTouchInt[1] = 0;
       data1 = 0;
       data2 = 0;
       result[0] = 0;
       result[1] = 0;
       statusTouch2 = false;
       statusTouch1 = false;
-      selectMode = mode[0];
-      dataC = 0;
 
-      print("up : " + x.round().toString());
+      _upCounter++;
+      print("up");
     });
   }
 
   void _updateLocation(PointerEvent details) {
     setState(() {
+      
       x = details.position.dx;
+      // y = details.position.dy;
+      // int data = x.toInt();
+      // print("x : " + data.toString());
       if (statusTouch1 && statusTouch2) {
         var jarak1 = 0;
         var jarak2 = 0;
-        jarak1 = (x - dataTouchInt[0]).abs().toInt();
-        jarak2 = (x - dataTouchInt[1]).abs().toInt();
-
-        // print("jarak 1 : " + jarak1.toString());
-        // print("jarak 2 : " + jarak2.toString());
+        jarak1 = (x - dataTouch[0]).abs().toInt();
+        jarak2 = (x - dataTouch[1]).abs().toInt();
 
         if (jarak1 > jarak2) {
           result[1] = x;
-          print("gerak kanan");
-          gKanan = true;
+          result[0] = dataTouch[0];
+          //result[0] = dataTouch[0].toDouble();
+          // result[0] = jarak1.toDouble();
         }
         if (jarak1 < jarak2) {
           result[0] = x;
-          gKiri = true;
+          result[1] = dataTouch[1];
+          // result[1] = jarak2.toDouble();
         }
-        print("Xnow : " + x.toInt().toString());
-        print("result 1 : " + result[0].toInt().toString());
-        print("result 2 : " + result[1].toInt().toString());
 
-        deltaTouch[0] = data1.toInt() - dataTouchInt[0].toInt();
-        deltaTouch[1] = dataTouchInt[1].toInt() - data2;
+        //  print("Touch 1 : " + result[0].toInt().toString());
+        // print("Touch 2 : " + result[1].toInt().toString());
+
+        //  if(result[0]<result[1]){
+
+        // data1 = result[0].toInt();
+        // data2 = result[1].toInt();
+
+        // }
+        // else{
+        //    data2 = result[0].toInt();
+        //     data1 = result[1].toInt();
+        // }
+
+        deltaTouch[0] = data1 - dataTouch[0].toInt();
+        deltaTouch[1] = dataTouch[1].toInt() - data2;
 
         if (result[0] < result[1]) {
           data1 = result[0].toInt();
           data2 = result[1].toInt();
-        } else {
+        } 
+        else {
           data2 = result[0].toInt();
           data1 = result[1].toInt();
         }
 
-        if (deltaTouch[0] != deltaTouch[1]) {
-          selectMode = mode[2];
-        }
-        if (jarak1 == jarak2) {
-          selectMode = mode[1];
-        } else {
-          dataC = 0;
-        }
-      }
+        // int data3,data4;
+        // if(data1 < data2){data3 = data1;data4=data2;}
+        // else{data3 = data2;data4=data1;}
 
-      if (selectMode == mode[1]) {
-        dataC = x.round() - dataTouch[0].round();
+        // data1 = data3;
+        // data2 = data4;
+
+        // distance = (data1 - data2).abs();
+
+         print("result 1 : " + result[0].toInt().toString());
+        print("result 2 : " + result[1].toInt().toString());
+
+        print("Data An : " + data1.toInt().toString());
+        // }
       }
     });
   }
 
   void enter(PointerEvent details) {
     setState(() {
-      // print("detailxx : " + details.position.dx.toString());
+      print("detailxx : " + details.position.dx.toString());
     });
   }
 
@@ -206,27 +197,19 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          Text("Mode : $selectMode\n\n"),
           Text("As : " +
               dataTouchInt[0].toString() +
               "   Bs : " +
               dataTouchInt[1].toString()),
+          Text("An : " + data1.toString() + "   Bn : " + data2.toString()),
 
-          Text("\nCs : " + dataTouch[0].round().toString()),
-          Text("\ndC : " + dataC.toString()),
-
-          Text("\nAn : " + data1.toString() + "   Bn : " + data2.toString()),
-
-          Text("\n\ndA : " +
-              deltaTouch[0].toString() +
-              "  dB : " +
-              deltaTouch[1].toString() +
-              "\n"),
+          Text("dA : " + deltaTouch[0].toString()),
+          Text("dB : " + deltaTouch[1].toString()),
           // Text("Distance : " + distance.toString()),
           // Text(),
 
           ConstrainedBox(
-            constraints: BoxConstraints.tight(Size(double.infinity, 50.0)),
+            constraints: BoxConstraints.tight(Size(300.0, 200.0)),
             child: Listener(
               onPointerDown: _incrementDown,
               onPointerMove: _updateLocation,
